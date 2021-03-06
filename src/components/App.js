d9ca7265f338420621a237d719e126f259a4c37a
JyothirmaiKakumanu <76103448+JyothirmaiKakumanu@users.noreply.github.com>
2021-03-06 22:21:57 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import "../styles/App.css";
 var timeInterval = 0;
 class Timer extends React.Component {
@@ -9,73 +9,70 @@ class Timer extends React.Component {
     this.handleListener = this.handleListener.bind(this);
   }
   // componentDidMount() {
-    
+
   // }
 
   // componentWillUnmount() {
-    
+
   // }
 
-  handleListener(event){
-    switch(event.keyCode){
+  handleListener(event) {
+    switch (event.keyCode) {
       case 39:
-        this.setState(
-          {x:this.state.x+5,}
-          );
+        this.setState({ x: this.state.x + 5 });
         break;
-      
+
       case 40:
-        this.setState(
-          {y:this.state.y+5,}
-          );
+        this.setState({ y: this.state.y + 5 });
         break;
-       
+
       case 38:
-        this.setState(
-          {y:this.state.y-5,}
-          );
+        this.setState({ y: this.state.y - 5 });
         break;
-        
+
       case 37:
-        this.setState(
-          {x:this.state.x-5,}
-          );
+        this.setState({ x: this.state.x - 5 });
+        break;
+      default:
         break;
     }
-    if(this.state.x==250 && this.state.y ==250){
-    
-      document.removeEventListener("keydown",this.handleListener);
+    if (this.state.x === 250 && this.state.y === 250) {
+      document.removeEventListener("keydown", this.handleListener);
       this.interval();
     }
   }
 
-  
-interval(){
-  if(this.state.time==0){
-    timeInterval = setInterval(()=>{this.setState({time: this.state.time+1})},1000);
+  interval() {
+    if (this.state.time === 0) {
+      timeInterval = setInterval(() => {
+        this.setState({ time: this.state.time + 1 });
+      }, 1000);
+    }
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(timeInterval);
+    }
   }
-  if(this.state.x==250 && this.state.y==250){
-    clearInterval(timeInterval);
-  }
-}
 
-displayBall(){
-  if(this.state.time==0){
-    this.interval();
-    document.addEventListener("keydown",this.handleListener);
+  displayBall() {
+    if (this.state.time === 0) {
+      this.interval();
+      document.addEventListener("keydown", this.handleListener);
+    }
   }
-  
-
-}
 
   render() {
     return (
- <>
- <div className="ball" style={{left : this.state.x, top : this.state.y}}></div>
- <div className="hole"></div>
- <button className="start" onClick={this.displayBall}>Start</button>
- <div className="heading-timer">{this.state.time}</div>
-</>
+      <>
+        <div
+          className="ball"
+          style={{ left: this.state.x, top: this.state.y }}
+        ></div>
+        <div className="hole"></div>
+        <button className="start" onClick={this.displayBall}>
+          Start
+        </button>
+        <div className="heading-timer">{this.state.time}</div>
+      </>
     );
   }
 }
