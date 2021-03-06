1f5dd14d73ce363f8b127de56848cc8edb4cf1f6
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-03-06 19:37:11 +0530

    committed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+var timeInterval = 0;
 class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: 0, x: 0, y: 0 };
+    this.displayBall = this.displayBall.bind(this);
+    this.handleListener = this.handleListener.bind(this);
   }
   // componentDidMount() {
     
@@ -39,18 +42,39 @@ class Timer extends React.Component {
           );
         break;
     }
+    if(this.state.x==250 && this.state.y ==250){
+    
+      document.removeEventListener("keydown",this.handleListener);
+      this.interval();
+    }
   }
 
+  
+interval(){
+  if(this.state.time==0){
+    timeInterval = setInterval(()=>{this.setState({time: this.state.time+1})},1000);
+  }
+  if(this.state.x==250 && this.state.y==250){
+    clearInterval(timeInterval);
+  }
+}
+
 displayBall(){
-  // if(this.state.time===0){
+  if(this.state.time==0){
+    this.interval();
     document.addEventListener("keydown",this.handleListener);
-  // }
+  }
+  
+
 }
 
   render() {
     return (
  <>
+ <div className="ball" style={{left : this.state.x, top : this.state.y}}></div>
+ <div className="hole"></div>
  <button className="start" onClick={this.displayBall}>Start</button>
+ <div className="heading-timer">{this.state.time}</div>
 </>
     );
   }
