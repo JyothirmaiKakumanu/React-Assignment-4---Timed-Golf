6efd28644dec1059e593cfa4cfbfe1e86ff0754b
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-03-06 18:48:37 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,52 @@ class Timer extends React.Component {
     super(props);
     this.state = { time: 0, x: 0, y: 0 };
   }
-  componentDidMount() {
+  // componentDidMount() {
     
+  // }
+
+  // componentWillUnmount() {
+    
+  // }
+
+  handleListener(event){
+    switch(event.keyCode){
+      case 39:
+        this.setState(
+          {x:this.state.x+5,}
+          );
+        break;
+      
+      case 40:
+        this.setState(
+          {y:this.state.y+5,}
+          );
+        break;
+       
+      case 38:
+        this.setState(
+          {y:this.state.y-5,}
+          );
+        break;
+        
+      case 37:
+        this.setState(
+          {x:this.state.x-5,}
+          );
+        break;
+    }
   }
 
-  componentWillUnmount() {
-    
-  }
-
-
+displayBall(){
+  // if(this.state.time===0){
+    document.addEventListener("keydown",this.handleListener);
+  // }
+}
 
   render() {
     return (
  <>
+ <button className="start" onClick={this.displayBall}>Start</button>
 </>
     );
   }
